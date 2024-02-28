6ca5b681223bfd34f415b60b75927a90224bc817
IM-EVAN <evanfister@outlook.com>
2024-02-27 21:28:17 -0700

    2/27 COMPONENTS AND SEMANTICS 	new file:   App.js 	new file:   Footer.js 	new file:   Header.js 	new file:   Main.js 	new file:   Nav.js

---
+++ App.js
@@ -0,0 +1,17 @@
+<>
+<header>
+    <image></image>
+</header>
+<nav.js>
+    <ul>
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>MENU</li>
+        <li>RESERVATIONS</li>
+        <li>ORDER ONLINE</li>
+        <li>LOGIN</li>
+    </ul>
+</nav.js>
+<Main.js></Main.js>
+<Footer.js></Footer.js>
+</>
